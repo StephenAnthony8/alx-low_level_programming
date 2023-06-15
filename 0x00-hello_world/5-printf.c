062b734cdbff555c5f6a02299a5b9ef48e20643d
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-15 15:45:05 +0300

    C files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main - prints 'with proper grammar, but the outcome is a piece of art,'
+ * Return: terminates the program's execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
