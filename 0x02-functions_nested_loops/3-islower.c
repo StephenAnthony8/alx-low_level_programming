e434282a897e17c3fbc7041bcf9bcadae271b203
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-20 17:31:31 +0300

    Mandatory C task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
  * _islower - checks for lowercase characters
- * @c: takes in the input value
- * Return: returns either 1 or zero depending on outcome
+ * @c: input for the function
+ * Return: returns either 1 or 0 depending on the state of the value input
  */
 int _islower(int c)
 {
