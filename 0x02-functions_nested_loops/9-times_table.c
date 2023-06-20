c69a6ad6d96916065e6e7d0b8ac351c9a1e24af3
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-21 00:04:01 +0300

    Mandatory C task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - creates a multiplication table upto 9x9
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = j * i;
+			if ((k / 10) != 48)
+			{
+				_putchar(((k / 10) + '0'));
+			}
+			_putchar(((k % 10) + '0'));
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
