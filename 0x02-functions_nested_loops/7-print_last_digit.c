869b49ecc15e463eb02f5e45424b98431c68101a
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-20 22:20:16 +0300

    Mandatory C task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - returns the last digit of a number
+ * @i: input integer
+ * Return: Returns the modulo value
+ */
+int print_last_digit(int i)
+{
+	if (i >= 0)
+	{
+		_putchar((i % 10) + '0');
+		return ((i % 10));
+	}
+	else
+	{
+		_putchar((-i % 10) + '0');
+		return ((-i % 10));
+	}
+
+}
