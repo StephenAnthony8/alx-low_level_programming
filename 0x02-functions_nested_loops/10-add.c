ef0305ee6625e46c1543390c5dfd46351411dbe7
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-21 09:58:59 +0300

    Mandatory C task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
- *
+ * add - adds two integers and returns the results
+ * Return: sum value of a & b
+ * @a: integer a
+ * @b: integer b
  */
 int add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
