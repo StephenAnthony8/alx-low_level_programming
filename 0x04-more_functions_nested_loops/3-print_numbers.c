3d1b3067b570b8e6b8e18641909f2ead811e01b8
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-22 12:33:48 +0300

    mandatory tasks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers 0 to 9
+ * print_most_numbers - prints numbers 0 to 9 except 2 & 4
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-		_putchar(i);
+		if ((i != '2') || (i != '4'))
+			_putchar(i);
 
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_numbers();
+    print_most_numbers();
     return (0);
 }
