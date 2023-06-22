de8339338743acd90f3aaec3512ea93b5a7ed6fb
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-22 13:51:34 +0300

    mandatory tasks

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints _ n times
+ * @n: number of times to print _
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (; n >= 0; n--)
+		{
+
+			while (i < 0)
+			{
+				_putchar('\\')
+			}
+		}
+	}
+	else
+		_putchar('\n');
+}
