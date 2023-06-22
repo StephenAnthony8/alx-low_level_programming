405833e5a3574ed9e28bd33fa747cff07cf41447
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-22 13:07:30 +0300

    mandatory tasks

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - prints _ n times
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
