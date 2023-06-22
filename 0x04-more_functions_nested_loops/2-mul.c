a4a12d134357f9d4153b3637d50721771fbc9890
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-22 12:15:50 +0300

    mandatory tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies int a & b
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns the multiplication values
+ */
+int mul(int a, int b)
+{
+	return ((a * b));
+}
