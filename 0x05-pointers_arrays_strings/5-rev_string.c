fed4b5c4271209d99db8ea9be8cb993cecc12eb3
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-26 17:29:09 +0300

    Mandatory task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_rev - prints out a string followed by a new line
+ * rev_string - prints out a reversed string followed by a new line
  * @s: string printed out
  */
 void rev_string(char *s)
 {
-	int i = 0;
-	char *ns;
+	int i = 0, j = 0;
+	char k;
 
 	while (s[i] != '\0')
+		i++;
+	i--;
+	while (j < i)
 	{
-		ns[i] = s[i];
+		k = s[j];
+		s[j] = s[i];
+		s[i] = k;
+
+		i--;
+		j++;
 	}
-		
+
 }
