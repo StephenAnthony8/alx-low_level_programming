278f7d9d53f15ed20a791234be2d613690d5059a
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-26 13:30:48 +0300

    Mandatory task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints out a string followed by a new line
+ * @s: string printed out
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (i != 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
