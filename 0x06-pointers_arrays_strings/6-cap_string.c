fb714df23783ae01cb940648bc396e64b21b6d69
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-29 23:08:30 +0300

    Mandatory tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,12 +6,11 @@
  */
 char *cap_string(char *n)
 {
-	char symbol[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n'};
+	char symbol[] = {',', ';', '.', '?', '"', '(', ')', '{', '}'};
 	int i, j, upper_val;
 
+	upper_val = 1;
 	i = 0;
-	upper_val = 0;
-
 	while (n[i] != '\0')
 	{
 		j = 0;
@@ -21,18 +20,22 @@ char *cap_string(char *n)
 				upper_val = 1;
 			j++;
 		}
+		if (n[i] >= 9 && n[i] <= 33)
+			upper_val = 1;
+
+		if (i != 0 && (n[i - 1] >= 'A' && n[i - 1] <= 'Z'))
+			upper_val = 0;
+		else if (i != 0 && (n[i - 1] >= '0' && n[i - 1] <= '9'))
+			upper_val = 0;
 
 		if (upper_val == 1)
-			if ((n[i] >= 'a') && (n[i] <= 'z'))
-				if ((n[i - 1] != 'A') && (n[i - 1] != 'Z'))
-				{
-					n[i] = n[i] - 32;
-					upper_val = 0;
-				}
-		if ((n[i] >= 'A') && (n[i] <= 'Z'))
-			upper_val = 0;
+			if(n[i] >= 97 && n[i] <= 122)
+			{
+				n[i] = n[i] - 32;
+				upper_val = 0;
+			}
 		i++;
+	
 	}
 	return (n);
-
 }
