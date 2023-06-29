995e09cb7d799b0eb92c9f468549cdc82f775f67
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-29 20:59:10 +0300

    Mandatory tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- *
+ * cap_string - Converts all Word beginnings to capital
+ * @n: string to be formatted
+ * Return: returns the formatted string
  */
 char *cap_string(char *n)
 {
-	char symbol[] = {',',';','.','!','?','"','(',')','{', '}', '\n'};
+	char symbol[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n'};
 	int i, j, upper_val;
+	
 	i = 0;
 	upper_val = 0;
 
@@ -14,17 +17,18 @@ char *cap_string(char *n)
 		j = 0;
 		while (symbol[j] != '\0')
 		{
-			if (n[i] == n[j])
+			if (n[i] == symbol[j])
 				upper_val = 1;
 			j++;
 		}
 
 		if (upper_val == 1)
-			if (((n[i] >= 'a') && (n[i] <= 'z')) && ((n[i -1] != 'A') && (n[i - 1] != 'Z')))
-			{
-				n[i] = n[i] - 32;
-				upper_val = 0;
-			}
+			if ((n[i] >= 'a') && (n[i] <= 'z'))
+				if ((n[i - 1] != 'A') && (n[i - 1] != 'Z'))
+				{
+					n[i] = n[i] - 32;
+					upper_val = 0;
+				}
 		if ((n[i] >= 'A') && (n[i] <= 'Z'))
 			upper_val = 0;
 		i++;
