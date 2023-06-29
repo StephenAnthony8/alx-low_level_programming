4b377b5b918e28aaee9078c94e7256b7265b91b1
StephenAnthony8 <anthonysteve02@gmail.com>
2023-06-29 16:03:13 +0300

    Mandatory tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - Reverses the array provided
+ * @n: number of elements in the array
+ * @a: array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, e;
+	int v;
+
+	i = 0;
+	e = n - 1;
+	while (i <= (n / 2))
+	{
+		v = a[i];
+		a[i] = a[e];
+		a[e] = v;
+		i++;
+		e--;
+	}
+}
