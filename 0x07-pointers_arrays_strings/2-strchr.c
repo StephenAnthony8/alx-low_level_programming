088959aa4b8a417538895982ca6765667e0e11f8
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-04 01:28:49 +0300

    Mandatory tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,12 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	for (;s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
-			break;
 		}
-		i++;
 	}
 	return (0);
 }
