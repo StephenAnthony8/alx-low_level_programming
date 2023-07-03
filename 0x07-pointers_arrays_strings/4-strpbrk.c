a36a2a78c069b6aef0edde32c615ddb3729bdd18
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-04 02:00:51 +0300

    Mandatory tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	i = 0;
-	for (; s[i] >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		j = 0;
-		for (; accept[j] >= '\0'; j++)
+		for (j = 0; accept[j] >= '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
