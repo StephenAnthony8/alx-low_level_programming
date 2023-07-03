1db318ffe4f27df1b56ad58b91cb941285038be6
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-04 02:48:16 +0300

    Mandatory tasks

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,24 @@
 #include "main.h"
 /**
- *
+ * _strstr - checks if string contains 'needle' subtring
+ * @haystack: str to be checked
+ * @needle: comparison str
+ * Return: string value
  */
 char *_strstr(char *haystack, char *needle)
 {
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *a = needle;
+		char *b = haystack;
+
+		while (*b == *a && *a != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*a == '\0')
+			return (haystack);
+	}
+	return (0);
 }
