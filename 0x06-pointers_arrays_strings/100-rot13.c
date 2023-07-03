10ea36d7d3ca06eb326d29eebfadb5cdd335ce41
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-03 15:10:45 +0300

    Recent push

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,18 +6,17 @@
  */
 char *rot13(char *n)
 {
-	int i, j;
+	int i;
 
 	i = 0;
 	while (n[i] != '\0')
 	{
-		j = 0;
-		while ((j != 25) && (n[i] >= 65 && n[i] <= 122))
+		while (n[i] >= 65 && n[i] <= 122)
 		{
 			if (((n[i] >= 'N') && (n[i] <= 'Z')) || ((n[i] >= 'n') && (n[i] <= 'z')))
 				n[i] = n[i] - 26;
 			n[i] = n[i] + 13;
-			j++;
+			break;
 		}
 		i++;
 	}
