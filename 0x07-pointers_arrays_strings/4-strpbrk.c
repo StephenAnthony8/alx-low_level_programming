3fd526d7b8e3ef656a49ef19c56a258341d18c27
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-04 01:43:38 +0300

    Mandatory tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,25 @@
 #include "main.h"
 /**
- *
+ * _strpbrk - points to first occurence of characters 'accept' in string 's'
+ * @s: String to be searched through
+ * @accept: Character being searched for
+ * Return: New string being returned
  */
 char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
 
+	i = 0;
+	for (; s[i] >= '\0'; i++)
+	{
+		j = 0;
+		for (; accept[j] >= '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (0);
+}
