0b00beeb23ea8d2e6dde490db5a8139100a98e4f
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-07 16:06:42 +0300

    Mandatory tasks

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds the commandline arguments
+ * @argc: array count
+ * @argv: commandline array
+ * Return: terminates the function
+ */
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	j = i = 1;
+	if (argc <= 1)
+	{
+		puts("0");
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (!strtol(argv[i], NULL, 10))
+		{
+			puts("Error");
+			return (1);
+		}
+		j *= strtol(argv[i], NULL, 10);
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
