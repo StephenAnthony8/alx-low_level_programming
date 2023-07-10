a94b1787c0c499f3460e1fde6e0d7cf21fe55d01
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-10 23:56:01 +0300

    Mandatory tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,12 +17,20 @@ int **alloc_grid(int width, int height)
 
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
+	{
 		return (NULL);
+		free(arr);
+	}
 	for (; i < width; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
 			return (NULL);
+		}
 	}
 
 	for (; j < height; j++)
