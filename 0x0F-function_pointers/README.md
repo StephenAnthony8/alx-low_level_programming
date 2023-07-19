20154b6bd6057740e20f07426f43a7d7915f8cf1
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-19 09:05:20 +0300

    Mandatory tasks

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+# 0x0F-function_pointers
+- Source files & header files answering 'function pointers' related questions.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/* Question 0 */
+void print_name(char *name, void (*f)(char *));
+
+/* Question 1 */
+
+/* Question 2 */
+
+/* Question 3 */
+
+/* Question 4 */
+
+#endif
