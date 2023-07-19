b5e46388d0ff416e9f0747c3981c875fd7b8b9a5
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-19 15:25:25 +0300

    Mandatory tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 		exit(100);
 	}
 	if ((argv[2][0] != '*') && (argv[2][0] != '%') && (argv[2][0] != '/')
-				&& (argv[2][0] != '-') && (argv[2][0] != '+'))
+				&& (argv[2][0] != '-') && (argv[2][0] != '+') && (argv[2][1] == '\0'))
 	{
 		puts("Error");
 		exit(99);
