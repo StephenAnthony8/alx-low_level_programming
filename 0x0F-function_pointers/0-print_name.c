e8f74478d15235a4805a69a3f21f2429eb5c1f77
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-19 12:41:10 +0300

    Mandatory tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints out a name using putchar
+ * @name: string to be printed out
+ * @f: function to be used for printing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*p)(char *) = f;
+
+	p(name);
+}
