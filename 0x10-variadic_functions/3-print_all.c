2e808c11e343d3878825ec1730bdd20bad638b21
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-21 01:34:57 +0300

    Mandatory tasks

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,29 +13,30 @@ void print_all(const char * const format, ...)
 	int i = 0;
 
 	va_start(args, format);
-	while (format[i] != '\0')
-	{
-		switch (format[i])
+	if (format)
+		while (format[i] != '\0')
 		{
-			case ('c'):
-				printf("%s%c", delimiter, va_arg(args, int));
-				break;
-			case ('i'):
-				printf("%s%d", delimiter, va_arg(args, int));
-				break;
-			case ('f'):
-				printf("%s%f", delimiter, va_arg(args, double));
-				break;
-			case ('s'):
-				r_str = va_arg(args, char *);
-				if (r_str == NULL)
-					r_str = "(nil)";
-				printf("%s%s", delimiter, r_str);
-				break;
+			switch (format[i])
+			{
+				case ('c'):
+					printf("%s%c", delimiter, va_arg(args, int));
+					break;
+				case ('i'):
+					printf("%s%d", delimiter, va_arg(args, int));
+					break;
+				case ('f'):
+					printf("%s%f", delimiter, va_arg(args, double));
+					break;
+				case ('s'):
+					r_str = va_arg(args, char *);
+					if (r_str == NULL)
+						r_str = "(nil)";
+					printf("%s%s", delimiter, r_str);
+					break;
+			}
+			i++;
+			delimiter = ", ";
 		}
-		i++;
-		delimiter = ", ";
-	}
 	printf("\n");
 	va_end(args);
 }
