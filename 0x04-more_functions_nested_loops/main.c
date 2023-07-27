e65850f0b4c9eef624ab51b3868663b59e65a75f
StephenAnthony8 <anthonysteve02@gmail.com>
2023-07-27 10:20:02 +0300

    update

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -7,15 +7,17 @@
  */
 int main(void)
 {
-    print_number(1);
+    print_number(98);
     _putchar('\n');
     print_number(402);
     _putchar('\n');
-    print_number(10240);
+    print_number(1024);
     _putchar('\n');
     print_number(0);
     _putchar('\n');
     print_number(-98);
+    _putchar('\n');
+    print_number(-98000);
     _putchar('\n');
     return (0);
 }
