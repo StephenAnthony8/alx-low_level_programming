25b6d9b9f3a803f13a7587955101796bccc8378e
StephenAnthony8 <anthonysteve02@gmail.com>
2023-08-01 15:47:23 +0300

    Mandatory tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,23 +5,34 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *tmp, *next_n, *prev_n;
+	listint_t *tmp, *count_n, *next_n, *prev_n;
 
-	tmp = *head;
-	for (i = 0; i < index && tmp; i++)
-	{
-		prev_n = tmp;
-		tmp = tmp->next;
-		if (tmp)
-			next_n = tmp->next;
-		else
-			return (-1);
-	}
-	if (i == 0)
+	count_n = tmp = *head;
+
+	if (*head == NULL)
+		return ( (index ? -1 : 1));
+	for (i = 0; count_n; i++)
+		count_n = count_n->next;
+	if (i < index)
 		return (-1);
-	prev_n->next = next_n;
+	if (index > 0)
+	{
+		for (i = 0; i < index; i++)
+		{
+			prev_n = tmp;
+			tmp = tmp->next;
+			if (tmp)
+				next_n = tmp->next;
+			else
+				next_n = NULL;
+		}
+		prev_n->next = next_n;
+	}
+	else
+	{
+		*head = tmp->next;
+	}
 	free(tmp);
+
 	return (1);
-
-
 }
