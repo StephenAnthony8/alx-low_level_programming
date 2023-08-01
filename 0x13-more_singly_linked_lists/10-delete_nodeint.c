d68a654fd9052886dc3d8a8ae45248c33a4f7278
StephenAnthony8 <anthonysteve02@gmail.com>
2023-08-01 16:02:10 +0300

    Mandatory tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	count_n = tmp = *head;
 
 	if (*head == NULL)
-		return ((index ? -1 : 1));
+		return (-1);
 	for (i = 0; count_n; i++)
 		count_n = count_n->next;
 	if (i < index)
