c73b0e079d1969b7fab959ebbe7f3ffa87be4a05
StephenAnthony8 <anthonysteve02@gmail.com>
2023-08-01 14:09:56 +0300

    Mandatory tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,9 +9,15 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *curr_n, *tmp, *new_n;/* *prev_n;*/
+	listint_t *curr_n, *tmp, *new_n, *count_n;
 
-	tmp = *head;
+	count_n = tmp = *head;
+
+	for (i = 0; count_n; i++)
+		count_n = count_n->next;
+	if (i < idx)
+		return (NULL);
+
 	new_n = malloc(sizeof(listint_t));
 	if (!new_n)
 		return (NULL);
@@ -22,8 +28,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			curr_n = tmp;
 			tmp = curr_n->next;
-			if (!tmp)
-				return (NULL);
 		}
 		curr_n->next = new_n;
 		new_n->next = tmp;
