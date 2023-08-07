49c9d6e42bc7542482714bc065c8f67629fe9528
StephenAnthony8 <anthonysteve02@gmail.com>
2023-08-08 00:46:07 +0300

    Mandatory tasks

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - creates or truncates existing files
+ * @filename: file to be created/modified
+ * @text_content: file content
+ * Return: execution value
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	i = fd = 0;
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_TRUNC);
+	if (fd == -1)
+	{
+		close(fd);
+		fd = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
+	}
+	if (fd < 0)
+		return (-1);
+	while (text_content[i])
+		i++;
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,9 @@ int _putchar(char c);
 
 /* question 0 */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* question 2 */
+int create_file(const char *filename, char *text_content);
+
+
 #endif
