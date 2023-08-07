dd8da44a7339c7915ee552e445b2532d701ced54
StephenAnthony8 <anthonysteve02@gmail.com>
2023-08-08 00:47:24 +0300

    Mandatory tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		fd = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
 	}
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
 	while (text_content[i])
 		i++;
