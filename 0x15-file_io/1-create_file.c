210d82c379a77d8959b1a30b89dcffe51f07be15
StephenAnthony8 <anthonysteve02@gmail.com>
2023-08-08 12:10:45 +0300

    Mandatory tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,22 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * create_file - creates or truncates existing files
+ * @filename: file to be created/modified
+ * @text_content: file content
+ * Return: execution value
  */
-int main(int ac, char **av)
+int create_file(const char *filename, char *text_content)
 {
-    int res;
+	int fd, i;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	i = fd = 0;
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_TRUNC);
+	if (fd == -1)
+	{
+		close(fd);
+		fd = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
+	}
+	if (fd == -1)
+		return (-1);
+	while (text_content[i])
+		i++;
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
 }
