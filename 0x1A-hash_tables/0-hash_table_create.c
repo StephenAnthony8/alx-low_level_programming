38be3d81b144374c6082ae9429b38ea9be645f67
StephenAnthony8 <steveoti.tony@gmail.com>
2023-10-06 10:52:16 +0300

    0-file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hsh_tab = NULL;
 	hash_node_t **hsh_arr = NULL;
+	unsigned long int i;
 	/* int size_p = 0;*/
 
 	/* size_p = sizeof(hash_node_t *); */
@@ -20,6 +21,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hsh_tab);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+	       hsh_arr[i] = NULL;
 	hsh_tab->size = size;
 	hsh_tab->array = hsh_arr;
 	return (hsh_tab);
