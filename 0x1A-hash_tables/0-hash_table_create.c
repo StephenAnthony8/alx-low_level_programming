9939823b0554babe16da0be383a0f7d9e3ee064b
StephenAnthony8 <steveoti.tony@gmail.com>
2023-10-06 07:30:20 +0300

    project 9

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table's cells
+ * Return: Null on failure or hash table pointer on success
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hsh_tab = NULL;
+	hash_node_t *hsh_arr = NULL;
+	/* int size_p = 0;*/
+
+	/* size_p = sizeof(hash_node_t *); */
+	hsh_tab = malloc(sizeof(hash_table_t));
+	if (!hsh_tab)
+		return (NULL);
+	hsh_arr = malloc(sizeof(hash_node_t *) * size);
+	if (!hsh_arr)
+	{
+		free(hsh_tab);
+		return (NULL);
+	}
+	hsh_tab->size = size;
+	hsh_tab->array = &(hsh_arr);
+	return (hsh_tab);
+}
